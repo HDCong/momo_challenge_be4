5fbe3faf9f6f779066e55f223e7c119f1dd0dc74
Cong <1712304@student.hcmus.edu.vn>
2020-10-01 13:48:19 +0700

    [COF] - handled on user connection

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const PORT = 8888
+
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient
-
+// base url to database
 var url = 'mongodb://localhost:27017/be4';
-
+// Connect to mongodb
 MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-    db.close();
-  }
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        console.log('Connection established to', url);
+        db.close();
+    }
 });
 
 app.get('/', (req, res) => {
@@ -24,10 +25,32 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log(`user with id ${socket.id} connected`);
+    console.log(`user with id ${socket.id} connected`);
+    
+    // handle when user emit login
+    socket.on('login', (username) => {
 
+    })
+
+    // when user emit find match
+    // find available room, if not exist, create join this user in new room 
+    socket.on('find-match',()=>{
+
+    })
+
+    // in game handle
+    // user emit chosen, compare and return point for user
+    socket.on('result',(chosen)=>{
+        console.log(chosen);
+    })
+
+    // return 100 highest user
+    socket.on('getRanking',()=>{
+
+    })
+    
 });
 
 http.listen(PORT, () => {
-  console.log(`listening on: ${PORT}`);
+    console.log(`listening on: ${PORT}`);
 });
